d2a14746de392ea2f1add520635c82e022c8aefc
cyrilpanicker <cyrilpanicker@gmail.com>
2017-03-09 16:47:29 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,59 @@
+angular.module('app',['ngTable'])
+.controller('MainController',function($scope,$http,ngTableParams){
+
+    $scope.usersTable = new ngTableParams({
+        page: 1,
+        count: 50,
+        pagerSeriesCount: 1,
+        sortable: false,
+        blockCaching: true,
+        paginate: false,
+        groupBy: null,
+        serverSideGrouping: false
+    }, {
+        getServerData: getUsers,
+        getData: getUsers
+    });
+
+    function getUsers($defer,params){
+        $http({method:'GET',url:'/users.json'}).then(function(response){
+            $defer.resolve(response.data,params);
+        });
+    }
+
+});
+
+// function getCachedAssignedJobs($defer, params) {
+//     $scope.jobQueueModel.isUpdating = true;
+//     if ($scope.jobQueueApiResponse.resultSet && $scope.jobQueueApiResponse.resultSet.length) {
+//         $defer.resolve($scope.jobQueueApiResponse);
+//         $scope.jobQueueModel.isUpdating = false;
+//     } else {
+//         getAssignedJobs($defer, params);
+//     }
+// }
+
+// function getAssignedJobs($defer, params) {
+//     $scope.jobQueueModel.isUpdating = true;
+//     var statusKey = $scope.jobQueueModel.selectedStatus ? $scope.jobQueueModel.selectedStatus.statusKey : null;
+//     myJobsFactory.getAssignedJobs({
+//         startPage: params.pagerData.startPage,
+//         endPage: params.pagerData.endPage,
+//         pageSize: $scope.config.assignedJobsPageSize,
+//         statusKey: statusKey
+//     }).then(function (data) {
+//         $scope.jobQueueModel.isUpdating = false;
+//         if (!data) {
+//             //TODO : go to error page
+//         } else if (!data.requests.length && !data.statuses.length) {
+//             //status is checked above since there could be requests existing for other statuses
+//             $state.go($scope.config.states.TABS_WITH_NO_JOBS_ASSIGNED); //show Start button (StartJob.html)
+//         } else {
+//             $scope.jobQueueModel.statuses = data.statuses;
+//             $scope.jobQueueApiResponse.resultSet = data.requests;
+//             $scope.jobQueueApiResponse.totalItems = data.totalRowCount;
+//             $defer.resolve($scope.jobQueueApiResponse);
+//             $scope.jobQueueModel.isUpdating = false;
+//         }
+//     });
+// }
